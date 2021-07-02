7aac9390f81ccd4434d0553147d091426ac03043
thangitus <thang.itus@gmail.com>
2021-07-02 21:59:41 +0700

    Refactor LocalNavMeshBuilder

--- Assets/AcquireChan/Scripts/LocalNavMeshBuilder.cs
+++ Assets/AcquireChan/Scripts/LocalNavMeshBuilder.cs
@@ -1,107 +1,124 @@
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.MixedReality.Toolkit;
+using Microsoft.MixedReality.Toolkit.SpatialAwareness;
 using UnityEngine;
 using UnityEngine.AI;
-using System.Collections;
-using System.Collections.Generic;
-using Microsoft.MixedReality.Toolkit.SpatialAwareness;
+using UnityEngine.EventSystems;
 using NavMeshBuilder = UnityEngine.AI.NavMeshBuilder;
-using SpatialAwarenessHandler = Microsoft.MixedReality.Toolkit.SpatialAwareness.IMixedRealitySpatialAwarenessObservationHandler<Microsoft.MixedReality.Toolkit.SpatialAwareness.SpatialAwarenessMeshObject>;
 
 // Build and update a localized navmesh from the sources marked by NavMeshSourceTag
-[DefaultExecutionOrder(-102)]
-public class LocalNavMeshBuilder : MonoBehaviour
-{
-    // The center of the build
-    public Transform m_Tracked;
+namespace AcquireChan.Scripts {
+    using SpatialAwarenessHandler = IMixedRealitySpatialAwarenessObservationHandler<SpatialAwarenessMeshObject>;
 
-    // The size of the build bounds
-    public Vector3 m_Size = new Vector3(80.0f, 20.0f, 80.0f);
+    public class LocalNavMeshBuilder : MonoBehaviour, SpatialAwarenessHandler {
+        // The center of the build
+        public Transform m_Tracked;
 
-    NavMeshData m_NavMesh;
-    AsyncOperation m_Operation;
-    NavMeshDataInstance m_Instance;
-    List<NavMeshBuildSource> m_Sources = new List<NavMeshBuildSource>();
+        // The size of the build bounds
+        public Vector3 m_Size = new Vector3(80.0f, 20.0f, 80.0f);
 
-    IEnumerator Start()
-    {
-        while (true)
-        {
+        NavMeshData m_NavMesh;
+        NavMeshDataInstance m_Instance;
+        private Dictionary<int, NavMeshBuildSource> sources = new Dictionary<int, NavMeshBuildSource>();
+
+        private void Start() {
             UpdateNavMesh(true);
-            yield return m_Operation;
-        }
-    }
-
-    void OnEnable()
-    {
-        // Construct and add navmesh
-        m_NavMesh = new NavMeshData();
-        m_Instance = NavMesh.AddNavMeshData(m_NavMesh);
-        if (m_Tracked == null)
-            m_Tracked = transform;
-        UpdateNavMesh(false);
-    }
-
-    void OnDisable()
-    {
-        // Unload navmesh and clear handle
-        m_Instance.Remove();
-    }
-
-    void UpdateNavMesh(bool asyncUpdate = false)
-    {
-        NavMeshSourceTag.Collect(ref m_Sources);
-        var defaultBuildSettings = NavMesh.GetSettingsByID(0);
-        var bounds = QuantizedBounds();
-
-        if (asyncUpdate)
-            m_Operation = NavMeshBuilder.UpdateNavMeshDataAsync(m_NavMesh, defaultBuildSettings, m_Sources, bounds);
-        else
-            NavMeshBuilder.UpdateNavMeshData(m_NavMesh, defaultBuildSettings, m_Sources, bounds);
-    }
-
-    static Vector3 Quantize(Vector3 v, Vector3 quant)
-    {
-        float x = quant.x * Mathf.Floor(v.x / quant.x);
-        float y = quant.y * Mathf.Floor(v.y / quant.y);
-        float z = quant.z * Mathf.Floor(v.z / quant.z);
-        return new Vector3(x, y, z);
-    }
-
-    Bounds QuantizedBounds()
-    {
-        // Quantize the bounds to update only when theres a 10% change in size
-        var center = m_Tracked ? m_Tracked.position : transform.position;
-        return new Bounds(Quantize(center, 0.1f * m_Size), m_Size);
-    }
-
-    void OnDrawGizmosSelected()
-    {
-        if (m_NavMesh)
-        {
-            Gizmos.color = Color.green;
-            Gizmos.DrawWireCube(m_NavMesh.sourceBounds.center, m_NavMesh.sourceBounds.size);
+            RegisterEventHandlers(this);
         }
 
-        Gizmos.color = Color.yellow;
-        var bounds = QuantizedBounds();
-        Gizmos.DrawWireCube(bounds.center, bounds.size);
+        private void OnEnable() {
+            // Construct and add navmesh
+            m_NavMesh = new NavMeshData();
+            m_Instance = NavMesh.AddNavMeshData(m_NavMesh);
+            if (m_Tracked == null)
+                m_Tracked = transform;
+            UpdateNavMesh();
+            RegisterEventHandlers(this);
+        }
 
-        Gizmos.color = Color.green;
-        var center = m_Tracked ? m_Tracked.position : transform.position;
-        Gizmos.DrawWireCube(center, m_Size);
+        #region register_event
+
+        private bool isRegistered;
+
+        private void RegisterEventHandlers(IEventSystemHandler handler) {
+            if (isRegistered || CoreServices.SpatialAwarenessSystem == null) return;
+            CoreServices.SpatialAwarenessSystem.RegisterHandler<SpatialAwarenessHandler>(handler);
+            isRegistered = true;
+        }
+
+        private void UnregisterEventHandlers(IEventSystemHandler handler) {
+            if (isRegistered || CoreServices.SpatialAwarenessSystem == null) return;
+            CoreServices.SpatialAwarenessSystem.RegisterHandler<SpatialAwarenessHandler>(handler);
+            isRegistered = true;
+        }
+
+        #endregion
+
+        private void OnDisable() {
+            m_Instance.Remove();
+            UnregisterEventHandlers(this);
+        }
+
+        private void OnDestroy() {
+            UnregisterEventHandlers(this);
+        }
+
+        private void UpdateNavMesh(bool asyncUpdate = false) {
+            var m_Sources = sources.Values.ToList();
+            var defaultBuildSettings = NavMesh.GetSettingsByID(0);
+            var bounds = QuantizedBounds();
+
+            if (asyncUpdate)
+                NavMeshBuilder.UpdateNavMeshDataAsync(m_NavMesh, defaultBuildSettings, m_Sources, bounds);
+            else
+                NavMeshBuilder.UpdateNavMeshData(m_NavMesh, defaultBuildSettings, m_Sources, bounds);
+        }
+
+        private static Vector3 Quantize(Vector3 v, Vector3 quant) {
+            var x = quant.x * Mathf.Floor(v.x / quant.x);
+            var y = quant.y * Mathf.Floor(v.y / quant.y);
+            var z = quant.z * Mathf.Floor(v.z / quant.z);
+            return new Vector3(x, y, z);
+        }
+
+        private Bounds QuantizedBounds() {
+            // Quantize the bounds to update only when theres a 10% change in size
+            var center = m_Tracked ? m_Tracked.position : transform.position;
+            return new Bounds(Quantize(center, 0.1f * m_Size), m_Size);
+        }
+
+        public void OnObservationAdded(MixedRealitySpatialAwarenessEventData<SpatialAwarenessMeshObject> eventData) {
+            var result = MeshFilter2NavMeshBuildSource(eventData.SpatialObject.Filter);
+            var id = eventData.SpatialObject.Id;
+            sources.Add(id, result);
+            UpdateNavMesh();
+        }
+
+        public void OnObservationUpdated(
+            MixedRealitySpatialAwarenessEventData<SpatialAwarenessMeshObject> eventData) {
+            var result = MeshFilter2NavMeshBuildSource(eventData.SpatialObject.Filter);
+            var id = eventData.SpatialObject.Id;
+            sources.Add(id, result);
+            UpdateNavMesh();
+        }
+
+        public void OnObservationRemoved(
+            MixedRealitySpatialAwarenessEventData<SpatialAwarenessMeshObject> eventData) {
+            var id = eventData.SpatialObject.Id;
+            sources.Remove(id);
+            UpdateNavMesh();
+        }
+
+        private static NavMeshBuildSource MeshFilter2NavMeshBuildSource(MeshFilter meshFilter) {
+            var mesh = meshFilter.mesh;
+            var navMeshBuildSource = new NavMeshBuildSource {
+                shape = NavMeshBuildSourceShape.Mesh,
+                sourceObject = mesh,
+                transform = meshFilter.transform.localToWorldMatrix,
+                area = 0
+            };
+            return navMeshBuildSource;
+        }
     }
-
-    // public void OnObservationAdded(MixedRealitySpatialAwarenessEventData<SpatialAwarenessMeshObject> eventData)
-    // {
-    //     UpdateNavMesh(true);
-    // }
-    //
-    // public void OnObservationUpdated(MixedRealitySpatialAwarenessEventData<SpatialAwarenessMeshObject> eventData)
-    // {
-    //     UpdateNavMesh(true);
-    // }
-    //
-    // public void OnObservationRemoved(MixedRealitySpatialAwarenessEventData<SpatialAwarenessMeshObject> eventData)
-    // {
-    //     UpdateNavMesh(true);
-    // }
 }
