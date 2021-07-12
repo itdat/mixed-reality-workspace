4625fbd9ef8ff63b57651e0a9f1201c3ddaaa344
thangitus <thang.itus@gmail.com>
2021-07-12 22:46:53 +0700

    Update freeze rotation objects

--- Assets/SpawnManager.cs
+++ Assets/SpawnManager.cs
@@ -5,10 +5,18 @@ public class SpawnManager : MonoBehaviour {
     [SerializeField]
     private GameObject[] listPrefabs;
 
+    public bool isInRoom;
+
     public void SpawnStuff(int id) {
         if (id < 0 || id > listPrefabs.Length) return;
-        var go = PhotonNetwork.Instantiate("Objects\\" + listPrefabs[id].name, gameObject.transform.position,
-            Quaternion.identity);
+        GameObject go;
+        var position = gameObject.transform.position;
+        position.z += 1;
+        if (isInRoom)
+            go = PhotonNetwork.Instantiate("Objects\\" + listPrefabs[id].name, position,
+                Quaternion.identity);
+        else
+            go = Instantiate(listPrefabs[id], position, Quaternion.identity);
         go.tag = "Stuff";
     }
 }
