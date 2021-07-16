838df3f689467e6580ebf1b38ca2ee6666bf001c
Nguyen Tuan Dat <53074785+itdat@users.noreply.github.com>
2021-07-16 08:32:38 +0700

    Add drawing on sphere feature

---
+++ Assets/KeyPressHandler.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Dialogflow;
+using UnityEngine;
+
+public class KeyPressHandler : MonoBehaviour
+{
+    [SerializeField] private DialogFlowClient client;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            client.OnButtonRecord();
+        }
+    }
+}
